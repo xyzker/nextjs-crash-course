2c4406b6a3cfa1143ecd403d6469a54c2bcbdf9a
xyzker <410317669@qq.com>
2026-01-04 10:52:07 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import events from "@/lib/constants";
+
+
 const Page = () => {
     return (
-        <div>Welcome to Next.js</div>
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event You Can&#39;t Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+            <ExploreBtn/>
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
     )
 }
 export default Page
